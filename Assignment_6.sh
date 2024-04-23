d16e4e09e1ed4bf3b473bb8d9d5e52035e80980c
Cmuraski <156373914+Cmuraski@users.noreply.github.com>
2024-04-23 17:40:09 -0400

    Add files via upload

---
+++ Assignment_6.sh
@@ -0,0 +1,24 @@
+15 splice variations in the human BRCA2 gene.
+27 exons in the longest splice variant.
+26 Introns in the longest splice variant.
+The BRCA2 gene is on chromosome 13.
+The gene synonyms of BRCA2: BRCC2, FAD, FACD, FANCD, FACD, FAD1, XRCC11
+In the African ostrich there is 1 ortholog of BRCA2
+The second exon sequence is: GCTCCTGGTTGTCTACCCATGGACCCAGAGGTTCTTTGACAGCTTTGGCAACCTGTCCTCTGCCTCTGCCATCATGGGCAACCCCAAAGTCAAGGCACATGGCAAGAAGGTGCTGACTTCCTTGGGAGATGCCACAAAGCACCTGGATGATCTCAAGGGCACCTTTGCCCAGCTGAGTGAACTGCACTGTGACAAGCTGCATGTGGATCCTGAGAACTTCAAG  
+The first intron of this sequence is 122 bsae pairs in length
+There are 723 pseudogenes
+ZARIL - BRCA2 - N4BP2L1
+rnf32 - sonic hedgehog gene- rmb33a
+There is only one copy of shh in betta fish.
+There are two copies of shh in atlantic salmon.
+26 Genome assemblies available in NCBI
+There are 11 genomes assembled into chromosomes
+There are 30 including the one mitochondrial chromosome
+Chromosome 1 is the longest
+There are 1115 miRNAs annotated
+Mari Kawaguchi et al "Purification and gene cloning of Fundulus heteroclitus hatching enzyme. A hatching enzyme system composed of high choriolytic enzyme and low choriolytic enzyme is conserved between two different teleosts, Fundulus heteroclitus and medaka Oryzias latipes"
+20: 1 mdllakasvl lllllslsna qtdnmeeaen gsskeeides eledvssiif rmnnnsmeel
+61 legdlvlpkt rnamkcfgap dscrwpkssn givkvpyvvs dnyesdeket irnamkefae
+121 ktcihfvprn neraylslep rfgcksmmgy vgdkqvvvlq rfgcikhavi qhellhalgf
+181 yhehtrsdrd qhvkinweni ikdfthnfdk ndtdnlgtpy dygsimhygr tafgkdrket
+241 itpipnpkaa igqtermsdi dilrvnklyk c
